b88e5f7b87da0e7c1cd0719380ee948130e6a77e
Nicole <67175550+Nicole1707@users.noreply.github.com>
2022-05-19 16:37:34 -0400

    update and load note

--- src/actions/notes.js
+++ src/actions/notes.js
@@ -1,25 +1,21 @@
 import { db } from "../firebase/firebase-config.js";
-import { collection, addDoc } from "firebase/firestore";
+import { collection, addDoc, doc, updateDoc } from "firebase/firestore";
 import { types } from "../components/types/types.js";
 import { loadNotes } from "../helpers/loadNotes.js";
-
 export const startNewNote = () => {
     return async (dispatch, getState) => {
-        const uid = getState().auth.uid;
+        const { uid } = getState().auth;
         const newNote = {
             title: '',
             body: '',
             date: new Date().getTime(),
         }
-        // const doc = await db.collection(`${uid}/journal/notes`).add(newNote); Antigua v8
-        //version 9
-
         const doc = await addDoc(collection(db, `${uid}/journal/notes`), newNote);
-       dispatch(activeNote(doc.id, newNote));
+        dispatch(activeNote(doc.id, newNote));
     }
 }
 
-export const activeNote =(id, note)=>(
+export const activeNote = (id, note) => (
     {
         type: types.notesActive,
         payload: {
@@ -30,17 +26,39 @@ export const activeNote =(id, note)=>(
 )
 
 export const startLoadingNotes = (uid) => {
-    return async (dispatch)=>{
-        const notes= await loadNotes(uid);
+    return async (dispatch) => {
+        const notes = await loadNotes(uid);
         dispatch(setNotes(notes));
-
     }
 }
 
 
 
-export const setNotes =(notes)=>({
+export const setNotes = (notes) => ({
 
     type: types.notesLoad,
     payload: notes
 });
+
+export const startSaveNote = (note) => {
+    return async (dispatch, getState) => {
+        const { uid } = getState().auth;
+
+        if (!note.url) {
+            delete note.url;
+        }
+
+        const noteTofiresotre = { ...note };
+        delete noteTofiresotre.id;
+        await updateDoc(doc(db, `/${uid}/journal/notes/${note.id}`), noteTofiresotre);
+
+        dispatch(refreshNote(note.id, note));
+
+    }
+
+}
+
+export const refreshNote = (id, note) => ({
+    type: types.notesUpdated,
+    payload: { id, note }
+})
--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useRef } from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { activeNote } from '../../actions/notes';
 import { useForm } from '../../hooks/useForm';
 import { NotesAppBar } from './NotesAppBar'
 
 export const NoteScreen = () => {
+    const dispatch = useDispatch();
     const { active: note } = useSelector(state => state.notes);
 
     const [formValues, handleInputChange, reset] = useForm(note);
@@ -18,6 +20,12 @@ export const NoteScreen = () => {
         }
 
     }, [note, reset]);
+
+    useEffect(() => {
+        dispatch(activeNote(formValues.id, { ...formValues }));
+        console.log(formValues);
+    }, [formValues, dispatch])
+
     return (
         <div className="notes__main-content">
             <NotesAppBar />
@@ -27,11 +35,13 @@ export const NoteScreen = () => {
                     placeholder="Some awesome title"
                     className="notes__title-input"
                     value={title}
+                    name="title"
                     onChange={handleInputChange} />
                 <textarea
                     placeholder="What happened today"
                     className="notes__textarea"
                     value={body}
+                    name="body"
                     onChange={handleInputChange}>
 
                 </textarea>
